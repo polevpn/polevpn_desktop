b0ce299ae030c6f68af92f5f47c3ff3aa04bda3d
star <star@gmail.com>
2021-11-09 09:59:13 +0800

    add windows ci

--- build_win.sh
+++ build_win.sh
@@ -1,6 +1,7 @@
 ./makerc.sh
 go mod tidy
 go build  -ldflags="-s -w -H windowsgui"
+rm polevpn.syso
 mkdir -p build/win/PoleVPN
 mkdir -p build/win/PoleVPN/service
 cp polevpn_desktop.exe build/win/PoleVPN/PoleVPN.exe
@@ -12,5 +13,4 @@ go mod tidy
 go build  -ldflags="-s -w"
 cp polevpn_service.exe ../build/win/PoleVPN/service
 cd ../build/win
-zip -r PoleVPN.zip ./* 
-rm polevpn.syso
+tar -a -c -f PoleVPN.zip ./*
