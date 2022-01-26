aa0213425ee966926ed49dff1f3ee5ebfcdb837c
team <team@gmail.com>
2022-01-26 11:08:45 +0800

    add code sign

--- build_macos.sh
+++ build_macos.sh
@@ -10,6 +10,7 @@ go mod tidy
 go build  -ldflags="-s -w"
 cp polevpn_service ../build/macos/PoleVPN.app/Contents/MacOS/service/
 cd ../build/macos
+codesign --force --deep --sign - PoleVPN.app
 zip -r PoleVPN.zip ./* 
 
 
