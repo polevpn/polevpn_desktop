fd5b185a7fab88f5414e9a9354fc1903cde89aaa
polevpn <polevpn@gmail.com>
2024-10-08 21:32:28 +0800

    install tuntap driver silently

--- build_win.sh
+++ build_win.sh
@@ -8,7 +8,7 @@ mkdir -p build/win/PoleVPN/service
 mkdir -p build/win/PoleVPN/deps
 cp polevpn_desktop.exe build/win/PoleVPN/PoleVPN.exe
 cp lib/Webview2Loader.dll build/win/PoleVPN
-cp -r resource/tap-windows.exe build/win/PoleVPN/deps
+cp -r resource/drivers build/win/PoleVPN/deps
 cp -r resource/MicrosoftEdgeWebview2Setup.exe build/win/PoleVPN/deps
 cp -r resource/init.bat build/win/PoleVPN
 cp version service/
--- resource/init.bat
+++ resource/init.bat
@@ -1,2 +1,29 @@
-deps\tap-windows.exe
+echo "install tuntap driver"
+
+deps\drivers\devcon.exe install deps\drivers\OemVista.inf tap0901
+
+reg query  "HKLM\SOFTWARE\WOW6432Node\Microsoft\EdgeUpdate\Clients\{F3017226-FE2A-4295-8BDF-00C3A9A7E4C5}"
+
+if %errorlevel% neq 0 goto NO
+if %errorlevel% equ 0 goto YES
+
+:NO
+
+reg query  "HKCU\Software\Microsoft\EdgeUpdate\Clients\{F3017226-FE2A-4295-8BDF-00C3A9A7E4C5}"
+
+if %errorlevel% neq 0 goto INSTALL
+if %errorlevel% equ 0 goto YES
+
+:YES
+echo Webview2Runtime installed
+goto END
+
+:INSTALL
+echo install webview2runtime
 deps\MicrosoftEdgeWebview2Setup.exe
+goto END
+
+:END
+
+
+
